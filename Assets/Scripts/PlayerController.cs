5b2f55519502f9064512fefff6bc6b7e833f34ad
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2025-07-22 21:16:23 +0900

    PlayerScript作業

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,79 +3,116 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("ˆÚ“®İ’è")]
-    [SerializeField,Header("ˆÚ“®‘¬“x")]
-    private float speed_ = 5f; 
-    private Vector2 inputMove;                  //“ü—Í‚³‚ê‚½ˆÚ“®•ûŒü 
-    //-----------ƒWƒƒƒ“ƒv------------
-    [SerializeField,Header("ƒWƒƒƒ“ƒv—Í")] 
-    private float jumpForce = 5f;
-    private Rigidbody rb;
-    private bool isGround = true;
-    [SerializeField] 
-    private LayerMask groundLayer;
-    //--------------Scriptƒ`ƒFƒ“ƒW--------------    
-    private PlayerController myScript;
-    private PlayerController myScript2;
-    [SerializeField] private GameObject player2;
-    [SerializeField] private bool isJump;
+    [Header("ç§»å‹•è¨­å®š")]
+    [SerializeField] private float speed_ = 5f;
+    private Vector2 inputMove;
 
+    [Header("ã‚¸ãƒ£ãƒ³ãƒ—è¨­å®š")]
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGround = false;
+
+
+    [SerializeField] private Vector3 groundCheckOffset = new Vector3(0f, -0.9f, 0f);
+    [SerializeField] private float groundCheckRadius = 0.3f;
+    private Vector3 groundCheckPosition;
+
+
+    [Header("ãƒ—ãƒ¬ã‚¤ãƒ¤ãƒ¼ã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆ")]
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    private PlayerController myScript1;
+    private PlayerController myScript2;
+
+    private GameObject activePlayer;
+    private Rigidbody activeRb;
+
+  
+    private void Awake()
+    {
+        activeRb = GetComponent<Rigidbody>();
+
+        myScript1 = player1.GetComponent<PlayerController>();
+        myScript2 = player2.GetComponent<PlayerController>();
+
+        SetActivePlayer(player1, myScript1);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        activeRb = GetComponent<Rigidbody>();
+    }
     public void OnMove(InputAction.CallbackContext context)
     {
-        //“ü—Í’l‚ğ“Ç‚İæ‚é
-        inputMove = context.ReadValue<Vector2>();  
+        //å…¥åŠ›å€¤ã‚’èª­ã¿å–ã‚‹
+        inputMove = context.ReadValue<Vector2>();
     }
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGround && isJump)
+        if (context.performed && isGround && activeRb != null)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            activeRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGround = false;
+
+            //Gizomosä½¿ç”¨
+            /* activeRb.linearVelocity = new Vector3(activeRb.linearVelocity.x,0,activeRb.linearVelocity.z);
+             activeRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+             isGround = false;*/
         }
     }
     public void OnChar1(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            myScript.enabled = false;
-            isJump = false;
+            SetActivePlayer(player1, myScript1);
         }
     }
     public void OnChar2(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            myScript.enabled = true;
-            isJump = true;
+            SetActivePlayer(player2, myScript2);
         }
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        myScript = GetComponent<PlayerController>();
-        myScript2 = player2.GetComponent<PlayerController>();
-        myScript2.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+
+      /*  groundCheckPosition = transform.position + groundCheckOffset;
+        isGround = Physics.CheckBox(groundCheckPosition, Vector3.one * groundCheckRadius, Quaternion.identity, groundLayer);*/
     }
     void Move()
     {
-        //Vector2‚©‚çVector3‚É•ÏX
-        Vector3 move = new Vector3 (inputMove.x, 0f ,inputMove.y) * speed_ * Time.deltaTime;
-        //Œ»İˆÊ’u‚É‰ÁZ‚µ‚ÄˆÚ“®
-        transform.position += move;
+        if (activePlayer != null && activeRb != null)
+        {
+            Vector3 move = new Vector3(inputMove.x, 0f, inputMove.y) * speed_ * Time.deltaTime;
+            activeRb.MovePosition(activeRb.position + move);
+        }
+    }
+    void SetActivePlayer(GameObject playerObj, PlayerController script)
+    {
+        player1.GetComponent<PlayerController>().enabled = false;
+        player2.GetComponent<PlayerController>().enabled = false;
+
+        script.enabled = true;
+        activePlayer = playerObj;
+        activeRb = activePlayer.GetComponent<Rigidbody>();
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (((1 << collision.gameObject.layer) & groundLayer) != 0)
         {
-           isGround = true;
+            isGround = true;
         }
     }
+
+    /*   private void OnDrawGizmosSelected()
+       {
+           Gizmos.color = isGround ? Color.green : Color.red;
+           Vector3 checkPosition = transform.position + groundCheckOffset;
+           Gizmos.DrawWireCube(checkPosition, Vector3.one * groundCheckRadius * 2f);
+       }*/
 }
